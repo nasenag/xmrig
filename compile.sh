e32831084fe703e4af08b34166b47cc774c8f153
nasenag16 <nasenag16@gmail.com>
2022-01-30 03:19:49 +0900

    add compile.sh

---
+++ compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd scripts
+./build_deps.sh && cd ../build
+cmake .. -DXMRIG_DEPS=scripts/deps
+make -j$(nproc)
+
+cp build/xmrig ../../SelfCompile-xmrig-new-build/
+cp src/config.json ../../SelfCompile-xmrig-new-build/
+
+
