9f7a02e8f0dea3ce6e01000d955ae87f8926e2cc
nasenag <nasenag16@gmail.com>
2022-02-02 18:40:14 +0900

    update compile

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ cd scripts
 cmake .. -DXMRIG_DEPS=scripts/deps
 make -j$(nproc)
 
-cp build/xmrig ../../SelfCompile-xmrig-new-build/
+cp xmrig ../../SelfCompile-xmrig-new-build/
 cp src/config.json ../../SelfCompile-xmrig-new-build/
 
 
